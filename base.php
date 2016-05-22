1c835d070609468e746861aa40b4aa43c43f9c0a
Mike iLL Kilmer <mike@mzoo.org>
2016-05-22 12:22:02 -0500

    Change name

--- base.php
+++ base.php
@@ -11,20 +11,20 @@ use Roots\Sage\Wrapper;
   <body <?php body_class(); ?>>
     <!--[if IE]>
       <div class="alert alert-warning">
-        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
+        <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'pure-demo'); ?>
       </div>
     <![endif]-->
     <?php
       do_action('get_header');
       get_template_part('templates/header');
     ?>
-    <div class="wrap container" role="document">
-      <div class="content row">
-        <main class="main">
+    <div class="wrap pure-g" role="document">
+      <div class="content">
+        <main class="main pure-u-1 pure-u-md-4-5">
           <?php include Wrapper\template_path(); ?>
         </main><!-- /.main -->
         <?php if (Setup\display_sidebar()) : ?>
-          <aside class="sidebar">
+          <aside class="sidebar pure-u-1 pure-u-md-1-5">
             <?php include Wrapper\sidebar_path(); ?>
           </aside><!-- /.sidebar -->
         <?php endif; ?>
--- templates/header.php
+++ templates/header.php
@@ -1,12 +1,19 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+<div id="header" class="header pure-u-1">
+  <header id="masthead" class="site-header" role="banner">
+    <div id="branding" class="site-branding">
+      <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+      <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+    </div>
+    <nav class="nav">
       <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
+      /**
+       * Only get the Primary menu if it is set in the WordPress Admin.
+       * The menu_class argument breaks if there is no menu activated in the WordPress Admin
+       */
+        if ( has_nav_menu( 'primary_navigation' ) ) :
+           wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-list']);
+        endif;
       ?>
     </nav>
-  </div>
-</header>
+  </header>
+</div>
