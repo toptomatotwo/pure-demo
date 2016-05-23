26f30066a7232e575e66e80bcdddb2da6eba7a1d
Mike iLL Kilmer <mike@mzoo.org>
2016-05-22 21:05:52 -0500

    Add footer menu

--- lib/setup.php
+++ lib/setup.php
@@ -27,7 +27,8 @@ function setup() {
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
   register_nav_menus([
-    'primary_navigation' => __('Primary Navigation', 'pure-demo')
+    'primary_navigation' => __('Primary Navigation', 'pure-demo'),
+    'footer_links' => __('Footer Links', 'pure-demo')
   ]);
 
   // Enable post thumbnails
--- templates/footer.php
+++ templates/footer.php
@@ -2,4 +2,15 @@
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+  <div id="colophon" class="site-footer" role="contentinfo">
+  <?php
+    if ( has_nav_menu( 'footer_links' ) ) :
+       wp_nav_menu(['theme_location' => 'footer_links', 'container_class' => 'pure-menu', 'menu_class' => 'pure-menu-list']);
+    endif;
+  ?>
+  </div>
+
+	<div class="site-info">
+		<?php printf( __( 'Site by %1$s.', 'pure-demo' ), '<a href="http://www.mzoo.org" rel="designer">Mike iLL/mZoo.org</a>',  ); ?>
+	</div>
 </footer>
