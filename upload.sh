7703866ffc4e7c40182b5114e7b16dee81896f1a
Mike iLL Kilmer <mike@mzoo.org>
2016-05-22 21:34:24 -0500

    Image screenshot.

--- upload.sh
+++ upload.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ $1 == --boo ]] ; then
-    rsync -avP inc screenshot.png templates *.php *.css elenaboo:public_html/wp/wp-content/themes/pure-demolition
+    rsync -avP dist lib screenshot.png templates *.php *.css elenaboo:public_html/wp/wp-content/themes/pure-demolition
 elif [[ $1 == --ill ]] ; then
-		rsync -avP inc screenshot.png templates *.php *.css bhujanga:public_html/wp/wp-content/themes/pure-coils
+		rsync -avP dist lib screenshot.png templates *.php *.css bhujanga:public_html/wp/wp-content/themes/pure-coils
 else
     echo Enter --boo or --ill
 fi
