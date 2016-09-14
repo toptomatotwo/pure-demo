97f6758b84cdb7055c4a6efe01344e34a97a8148
Mike iLL Kilmer <mike@mzoo.org>
2016-09-14 14:14:44 -0500

    Typographical updates and add new location to upload.

--- upload.sh
+++ upload.sh
@@ -4,8 +4,8 @@ if [[ $1 == --boo ]] ; then
     rsync -avP dist lib screenshot.png templates *.php *.css elenaboo:public_html/wp/wp-content/themes/pure-demolition
 elif [[ $1 == --ill ]] ; then
 		rsync -avP dist lib screenshot.png templates *.php *.css bhujanga:public_html/wp/wp-content/themes/pure-coils
+elif [[ $1 == --my ]] ; then
+		rsync -avP dist lib screenshot.png templates *.php *.css my:public_html/wprs/wp-content/themes/pure-yoga
 else
-    echo Enter --boo or --ill
+    echo Enter --boo or --ill or --my
 fi
-
-
